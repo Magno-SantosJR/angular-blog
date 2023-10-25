bc0007ea16c47007920d355aed9acd9a446b380c
Magno-SantosJR <ma9nojr@gmail.com>
2023-10-25 16:47:25 -0300

    news

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
-
+  photoCover: string = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  cardTitle: string = "NOVO HOMEM DE FERRO ANUNCIADO"
+  cardDescription: string = "Marvel Studios anuncia novo filme do homem de ferro."
   constructor() { }
 
   ngOnInit(): void {
