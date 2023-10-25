66f00fd6b9c65c02b1ac7a3e307c77a515b47884
Magno-SantosJR <ma9nojr@gmail.com>
2023-10-25 20:56:08 -0300

    content created

--- src/app/Data/dataFake.ts
+++ src/app/Data/dataFake.ts
@@ -8,7 +8,19 @@ export const dataFake = [
     {
         "id": "2",
         "photo": "https://s2-g1.glbimg.com/SQa931aXAu-DBdiTkc0lB9XGegE=/0x0:1170x658/810x456/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/S/2/A9Xa1hRrWLYpOTYXWxsg/vacina-bivalente-contra-a-covid-19-e-ampliada-para-pessoas-a-partir-de-18-anos-em-cidades-do-vale-do-paraiba.jpg",
-        "title": "Saiu novo filme 2",
-        "description": "olá 2"
+        "title": "Casos de Covid no estado aumentam 10 vezes em 12 semanas",
+        "description": "Vigilância Epidemiológica reforça vacinação e medidas de prevenção, como a etiqueta da tosse."
+    },
+    {
+        "id": "3",
+        "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "Como a Marvel decide quais personagens ganharão uma série própria no Disney+?",
+        "description": "Você já se perguntou quais personagens da Marvel terão suas próprias histórias ou novas aventuras contadas nos cinemas ou em séries no Disney+? Pois foi exatamente esse assunto que Kevin Feige, o presidente da Marvel Studios, abordou em uma recente entrevista ao Rotten Tomatoes, conceituado portal de críticas de filmes e séries. Na edição de …." 
+    },
+    {
+        "id": "4",
+        "photo": "../../../assets/noticia cachorro.png",
+        "title": "🐶Creche de pets viraliza ao oferecer serviço de ônibus 'escãolar'",
+        "description": "Com vídeos publicados nas redes sociais que ultrapassaram 3 milhões de visualizações, dono de estabelecimento divulga o trajeto dos animais." 
     }
 ]
